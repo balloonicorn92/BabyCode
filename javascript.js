6277578870b3cfe60d7692b3c669b48560ec9d54
Fernanda Frers <67517589+balloonicorn92@users.noreply.github.com>
2021-01-24 13:07:59 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function myFunctiongrow() {
+    document.getElementById("box").style.height = "200px"; 
+    document.getElementById("box").style.width = "200px";
+}
+function myfunctionchange() {
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+function myfunctionfade() {
+    document.getElementById("box").style.opacity = "0.5";
+}
+function myfunctionreset() { 
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1";
+}
